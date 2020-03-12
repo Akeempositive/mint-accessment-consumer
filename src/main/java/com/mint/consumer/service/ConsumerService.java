333f57251e5328fb5f6a280d35f1032bf502bee2
asuara <asuara@teamapt.com>
2020-03-12 03:52:59 +0100

    serializing string response from kafka to usable object in consumer (VerifyCard Response Object)

--- src/main/java/com/mint/consumer/service/ConsumerService.java
+++ src/main/java/com/mint/consumer/service/ConsumerService.java
@@ -1,5 +1,7 @@
 package com.mint.consumer.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mint.consumer.model.VerifyCardResponse;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
@@ -10,10 +12,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class ConsumerService {
 
+    ObjectMapper objectMapper = new ObjectMapper();
+
     @KafkaListener(topics = "com.ng.vela.even.card_verified")
     public void listenWithHeaders(
             @Payload String received,
-            @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
+            @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) throws JsonProcessingException {
+        VerifyCardResponse verifyCardResponse = objectMapper.readValue(received, VerifyCardResponse.class);
         System.out.println(
                 "Received Message: succesful " + received
                         + "from partition: " + partition);
